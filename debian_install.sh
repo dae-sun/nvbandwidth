b0f61c80dcfcb44b108903a3083ae5bb3784e7f8
ㅎㅎ <67183356+dae-sun@users.noreply.github.com>
2024-03-13 19:55:36 +0900

    Update debian_install.sh

--- debian_install.sh
+++ debian_install.sh
@@ -2,10 +2,7 @@
 # Utility script that attempts to install
 # necessary software components needed to
 # build nvbandwidth
-
-apt install build-essential
-apt install libboost-program-options-dev
-apt install cmake
+apt install -y libboost-program-options-dev && build-essential && cmake
 output=$(cmake --version | sed -n 1p | sed 's/[^0-9]*//g')
 if [ $output -lt 3200 ]; then
     echo "Upgrade cmake version to 3.20 or above to build nvbandwidth"
